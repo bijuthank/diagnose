157d148afc44fe11cb74188dcad20aa4b69c7051
Biju Thankachan <bijuthank@users.noreply.github.com>
2019-09-27 22:23:57 +0300

    updated files

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import getLogger
 
-__author__ = 'Lucas Vogel'
+__author__ = 'Biju Thankachan'
 LOGGER = getLogger(__name__)
 
 
@@ -16,9 +16,9 @@ class Diagnose(MycroftSkill):
     def handle_Diagnose_intent(self, message):
         decision = self.ask_yesno("disease")
         if decision == "yes":
-            d2 = self.ask_yesno("otherInformation")
+            d2 = self.ask_yesno("COPDprognosis")
             if d2 == "yes":
-                d3 = self.ask_yesno("COPDprognosis")
+                d3 = self.ask_yesno("therapy")
             elif d2 == "no":
                 self.speak_dialog("anythingElse")
         elif desicion == "no":
