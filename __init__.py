ba9645e50653cec5506f403bea653adb2b75922d
Biju Thankachan <bijuthank@users.noreply.github.com>
2019-09-27 16:18:05 +0300

    summary

--- __init__.py
+++ __init__.py
@@ -5,16 +5,58 @@ from mycroft.util.log import getLogger
 __author__ = 'Lucas Vogel'
 LOGGER = getLogger(__name__)
 
-class HelpMeSkill(MycroftSkill):
+class DiagnoseMeSkill(MycroftSkill):
+    @intent_handler(IntentBuilder("").require("diagnose.invocation"))
+    def handle_DiagnoseMe_intent(self, message):
+        self.speak_dialog("disease", expect_response=True)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################################################################################################################
 
     def get_user_response(self, dialog):
         response = self.get_response(diagnose.invocation)
-        return response
+    -    return response
 
     @intent_handler(IntentBuilder("").require("diagnose.invocation"))
     def handle_knock_knock_intent(self, message):# They said help me
         #ask for body part
-        bodypart = self.get_user_response("disease")
+        user_response = self.get_user_response("disease")
+
+        if user_response == "Yes" or  user_response =="yes":
+            user_response = self.get_user_response (# write the dialog name where interested is stored)
+        elif user_response == "No" or user_response == "no":
+            self.speak_dialog ("OK, can I help you with anything else")
+        elif user_response 
+        
+
+
+
+
+
+
+
+
         # They said the body part
         specificypart = self.get_user_response("what.exactly")
         #they specify the body part
