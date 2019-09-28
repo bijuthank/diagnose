e86cf806ba3830bdacb32bcaef9ca03a009ba415
Biju Thankachan <bijuthank@users.noreply.github.com>
2019-09-28 12:27:16 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,13 +8,10 @@ LOGGER = getLogger(__name__)
 
 class Diagnose(MycroftSkill):
 
-    def get_user_response(self, dialog):
-        response = self.get_response(dialog)
-        return response
-
+    
     @intent_handler(IntentBuilder("").require("disease.details"))
     def handle_diagnose_intent(self, message):
-        self.speak("disease")
+        self.speak_dialog("disease")
         
    
 def create_skill():
