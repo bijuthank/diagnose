dcb5b1631dc275d8b63880e51bc5de94b0be3659
Biju Thankachan <bijuthank@users.noreply.github.com>
2019-09-28 12:19:19 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,16 +13,9 @@ class Diagnose(MycroftSkill):
         return response
 
     @intent_handler(IntentBuilder("").require("disease.details"))
-    def handle_Diagnose_intent(self, message):
-        decision = self.ask_yesno("disease")
-        if decision == "yes":
-            d2 = self.ask_yesno("COPDprognosis")
-            if d2 == "yes":
-                d3 = self.ask_yesno("therapy")
-            elif d2 == "no":
-                self.speak_dialog("OKThankYou")
-        elif desicion == "no":
-            self.speak_dialog("OKThankYou")
+    def handle_diagnose_intent(self, message):
+        self.speak_dialog("disease")
+        
    
 def create_skill():
     return Diagnose()
