07df0047839df187e6e50213a825756638e5c92f
Biju Thankachan <bijuthank@users.noreply.github.com>
2019-09-28 13:10:41 +0300

    update

--- __init__.py
+++ __init__.py
@@ -5,15 +5,21 @@ from mycroft.util.log import getLogger
 __author__ = 'Biju Thankachan'
 LOGGER = getLogger(__name__)
 
+class HelpMeSkill(MycroftSkill):
 
-class Diagnose(MycroftSkill):
+    def get_user_response(self, dialog):
+        response = self.get_response(dialog)
+        return response
 
-    
     @intent_handler(IntentBuilder("").require("disease.details"))
-    def handle_diagnose_intent(self, message):
+    def handle_knock_knock_intent(self, message):# They said help me
         self.speak_dialog("disease")
-        
-   
+# class Diagnose(MycroftSkill):
+
+#     @intent_handler(IntentBuilder("").require("disease.details"))
+#     def handle_diagnose_intent(self, message):
+#         self.speak_dialog("disease")
+
 def create_skill():
     return Diagnose()
 
