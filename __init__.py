26b1c9657b69ecf52d2ecf920536537d5a00927c
Biju Thankachan <bijuthank@users.noreply.github.com>
2019-09-28 13:34:47 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,26 +11,11 @@ class HelpMeSkill(MycroftSkill):
         response = self.get_response(dialog)
         return response
 
-
-
-
     @intent_handler(IntentBuilder("").require("disease.details"))
     def handle_disease_intent(self, message):
         self.speak_dialog("disease")
 
-    @intent_handler(IntentBuilder("").require("copd.details"))
-     def handle_copd_intent(self, message):
-         self.speak_dialog("copd")
-
-    @intent_handler(IntentBuilder("").require("therapy.details"))
-     def handle_therapy_intent(self, message):
-         self.speak_dialog("therapy") 
-        #self.speak_dialog("disease")
-# class Diagnose(MycroftSkill):
-
-#     @intent_handler(IntentBuilder("").require("disease.details"))
-#     def handle_diagnose_intent(self, message):
-#         self.speak_dialog("disease")
+    
 
 def create_skill():
     return Diagnose()
